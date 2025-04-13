bad32d33b0030af5d4236928529bd43d8c3295b9
PiesAvalon <tommy.ge@qq.com>
2025-04-13 14:21:54 +0800

    report03

--- computer_organization/report/report03/script.py
+++ computer_organization/report/report03/script.py
@@ -46,7 +46,7 @@ def save_markdown_to_file(markdown_table, output_file):
 
 def main():
     # 输入和输出文件路径
-    input_file = r".\report01\真值表\zhenzhi5.txt"  # 替换为你的输入文件路径
+    input_file = r".\computer_organization\report\report03\test.txt"  # 替换为你的输入文件路径
     output_file = "output_table.md"  # 替换为你想要的输出文件路径
     
     try:
